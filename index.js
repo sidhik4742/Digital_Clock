827684d4a54ef48ad1e47c68c8d0fed079fce577
sidhik4742 <sidhik64742@gmail.com>
2020-09-25 19:32:32 +0530

    #ready to work with sound button

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const date = require("date-and-time");
+
 const pattern = date.compile("dddd MMM DD YYYY hh mm ss A");
 
-setInterval(function () {
-  var now = new Date();
+const newDate = () => {
+  let now = new Date();
   let currentTime = date.format(now, pattern);
+  return currentTime;
+};
+
+const speaking = (time) => {
+  console.log(time);
+};
+
+setInterval(function () {
+  let currentTime = newDate();
   // console.log(currentTime);
   currentTime = currentTime.split(" ");
   document.getElementById("ampm").innerHTML = currentTime[7];
@@ -18,6 +28,34 @@ setInterval(function () {
     "," +
     currentTime[3];
 }, 1000);
+
+document.getElementById("speaker").addEventListener("click", (event) => {
+  // https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl=en&q=Thetimeis+,+4+50
+  // var currentTime = newDate();
+  event.preventDefault();
+
+  let currentTime = document.getElementById("time").innerText;
+  let ampm = document.getElementById("ampm").innerText;
+
+  console.log(currentTime);
+  console.log(ampm);
+  // *! Thursday Sep 24 2020 /////12 35 02///// PM //
+  currentTime = currentTime.split(":");
+
+  var time = `The time is,${currentTime[0]},${currentTime[1]} `;
+  console.log(time);
+  // console.log(typeof time);
+
+  //*! The time is,01,01///////
+  let url = `https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl=en&q=${time}${ampm}`;
+  url = encodeURI(url);
+
+  let audio = document.createElement("AUDIO");
+  audio.setAttribute("src",url);
+  audio.play(); 
+
+  console.log(url);
+});
 
 /*
 0: "Saturday"
--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-
 const { app, BrowserWindow } = require("electron");
+const say = require("say");
 
 function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
     width: 350,
-    height: 150,
-    frame: false,
+    height: 200,
+    frame: true,
     webPreferences: {
       nodeIntegration: true,
     },
@@ -22,6 +22,7 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
+app.commandLine.appendSwitch('enable-speech-dispatcher');
 app.whenReady().then(createWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
