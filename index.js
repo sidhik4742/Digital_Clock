43398fde3e46262ae43fccbaaed4fdb66e6f16b6
sidhik4742 <sidhik64742@gmail.com>
2020-09-26 17:44:53 +0530

    # added feature 1) added hour speak- talk every hour.

--- index.js
+++ index.js
@@ -2,16 +2,44 @@ const date = require("date-and-time");
 
 const pattern = date.compile("dddd MMM DD YYYY hh mm ss A");
 
+//*? Genarate and format date/////////
 const newDate = () => {
   let now = new Date();
   let currentTime = date.format(now, pattern);
   return currentTime;
 };
 
-const speaking = (time) => {
-  console.log(time);
+//*? created a audio element in the dom and play the audio string//////
+const speaking = (url) => {
+  console.log(url);
+  let audio = document.createElement("AUDIO");
+  audio.setAttribute("src", url);
+  audio.play();
+
+  console.log(url);
 };
 
+//*? Take value frome the dom and create url////////
+const createUrl = () => {
+  let currentTime = document.getElementById("time").innerText;
+  let ampm = document.getElementById("ampm").innerText;
+
+  console.log(currentTime);
+  console.log(ampm);
+  // *! Thursday Sep 24 2020 /////12 35 02///// PM //
+  currentTime = currentTime.split(":");
+
+  var time = `The time is,${currentTime[0]},${currentTime[1]} `;
+  console.log(time);
+  // console.log(typeof time);
+
+  //*! The time is,01,01///////
+  let url = `https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl=en&q=${time}${ampm}`;
+  url = encodeURI(url);
+  return url;
+};
+
+//*? update dom element every 1 sec/////////
 setInterval(function () {
   let currentTime = newDate();
   // console.log(currentTime);
@@ -29,32 +57,22 @@ setInterval(function () {
     currentTime[3];
 }, 1000);
 
+//*? hour timer set to every hour and automatically talk time///////////
+setInterval(function () {
+  let currentTime = document.getElementById("time").innerText;
+  currentTime = currentTime.split(":");
+  if (currentTime[1] == "00") {
+    console.log("This is the one hour timer");
+    speaking(createUrl());
+  }
+}, 60 * 1000);
+
+//*? an event is trigger when pressing a speaker icon/////////
 document.getElementById("speaker").addEventListener("click", (event) => {
   // https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl=en&q=Thetimeis+,+4+50
   // var currentTime = newDate();
   event.preventDefault();
-
-  let currentTime = document.getElementById("time").innerText;
-  let ampm = document.getElementById("ampm").innerText;
-
-  console.log(currentTime);
-  console.log(ampm);
-  // *! Thursday Sep 24 2020 /////12 35 02///// PM //
-  currentTime = currentTime.split(":");
-
-  var time = `The time is,${currentTime[0]},${currentTime[1]} `;
-  console.log(time);
-  // console.log(typeof time);
-
-  //*! The time is,01,01///////
-  let url = `https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl=en&q=${time}${ampm}`;
-  url = encodeURI(url);
-
-  let audio = document.createElement("AUDIO");
-  audio.setAttribute("src",url);
-  audio.play(); 
-
-  console.log(url);
+  speaking(createUrl());
 });
 
 /*
